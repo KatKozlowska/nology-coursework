591d07fdf12dda46e839d5965cfb75e4a2781ece
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2023-11-12 20:18:03 +0000

    cleaned up code

--- 03-Javasript/challenge-javascript/final-challenge.js
+++ 03-Javasript/challenge-javascript/final-challenge.js
@@ -2,7 +2,7 @@
 // 1. Function which takes an number and returns true if its positive and false if its negative.
 
 const isNumberPositive = (number) => {
-    if (number>0) {
+    if (number > 0) {
         return true;
     } else { 
         return false;
@@ -17,7 +17,7 @@ console.log(isNumberPositive(10));
 
 
 const convertDaysToAge = (days) =>{   
-    const years= days/365;
+    const years = days / 365;
     const message = days + " days is equal to " + years + " years.";
     return message;
 }
@@ -26,20 +26,20 @@ console.log(convertDaysToAge(6570));
 
 // 3. Function which takes three numbers and returns the largest of 3. 
 
-const getLargestNumber= (num1,num2,num3) => {
-     if (num1>num2 && num1>num3) {
-        return num1 ;
-    } else if (num2>num1 && num2>num3) {
+const getLargestNumber = (num1, num2, num3) => {
+     if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
         return num2;
-    } else if (num3>num1 && num3>num2) {
+    } else if (num3 > num1 && num3 > num2) {
         return num3; 
     } 
    
     return "All the numbers are equal";
 } 
 
-console.log(getLargestNumber(2, 1 ,4));
-console.log(getLargestNumber(6, 2 ,3));
+console.log(getLargestNumber(2, 1, 4));
+console.log(getLargestNumber(6, 2, 3));
 
 // 4. Function that takes an array of names and returns the last name from the array of names. 
 
@@ -48,7 +48,7 @@ const getLastName = (nameArr) => {
 }
 
 console.log(getLastName(["Charlie", "Rob", "Andy"]));
-console.log(getLastName(["Ash", "Stu",]));
+console.log(getLastName(["Ash", "Stu"]));
 
 
 // or ...
@@ -63,7 +63,7 @@ const getLastNameRecursively = (nameArr) => {
 }
 
 console.log(getLastNameRecursively(["Charlie", "Rob", "Andy"]));
-console.log(getLastNameRecursively(["Ash", "Stu",]));
+console.log(getLastNameRecursively(["Ash", "Stu"]));
 
 
 
@@ -71,7 +71,7 @@ console.log(getLastNameRecursively(["Ash", "Stu",]));
 // one or more negative numbers in the array. 
 
 const allNUmbersPositive = (numberArr) => {
-    for( let index = 0; index < numberArr.length; index++) {
+    for (let index = 0; index < numberArr.length; index++) {
         if (numberArr[index] < 0) {
             return false; 
         }
@@ -79,5 +79,5 @@ const allNUmbersPositive = (numberArr) => {
     return true;
 }
 
-console.log(allNUmbersPositive([2,4,5]))
-console.log(allNUmbersPositive([-5,4,6]))
+console.log(allNUmbersPositive([2, 4, 5]));
+console.log(allNUmbersPositive([-5, 4, 6]));
