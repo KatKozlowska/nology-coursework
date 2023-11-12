359d5b3a8cdcb583fc9967081114d64ac7e3672f
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2023-11-12 20:12:48 +0000

    finshed challenge tasks

--- 03-Javasript/challenge-javascript/final-challenge.js
+++ 03-Javasript/challenge-javascript/final-challenge.js
@@ -3,58 +3,81 @@
 
 const isNumberPositive = (number) => {
     if (number>0) {
-    console.log("true");
-    } else (number<0) 
-    console.log("false");
+        return true;
+    } else { 
+        return false;
+    }
 }
 
-isNumberPositive(-1);
-isNumberPositive(10);
+console.log(isNumberPositive(-1));
+console.log(isNumberPositive(10));
 
-// why is this returning three resutlts instead of two???
 
 // 2. Function which takes a number of days and converts it to age. 
 
 
 const convertDaysToAge = (days) =>{   
-    const years= days/365
-    const message = days + " days is equal to " + years + " years."
-    console.log(message) 
+    const years= days/365;
+    const message = days + " days is equal to " + years + " years.";
+    return message;
 }
-convertDaysToAge(3650);
-convertDaysToAge(6570);
+console.log(convertDaysToAge(3650));
+console.log(convertDaysToAge(6570));
 
 // 3. Function which takes three numbers and returns the largest of 3. 
 
 const getLargestNumber= (num1,num2,num3) => {
-     if (num1>num2 && num1>num3){
-     console.log (num1);
-    } else if (num2>num1 && num2>num3){
-    console.log (num2);
-    }else if (num3>num1 && num3>num2){
-    console.log (num3); 
-    } else console.log("All the numbers are equal");
+     if (num1>num2 && num1>num3) {
+        return num1 ;
+    } else if (num2>num1 && num2>num3) {
+        return num2;
+    } else if (num3>num1 && num3>num2) {
+        return num3; 
+    } 
+   
+    return "All the numbers are equal";
 } 
 
-getLargestNumber(2, 1 ,4);
-getLargestNumber(6, 2 ,3);
+console.log(getLargestNumber(2, 1 ,4));
+console.log(getLargestNumber(6, 2 ,3));
 
 // 4. Function that takes an array of names and returns the last name from the array of names. 
 
+const getLastName = (nameArr) => {
+    return nameArr[nameArr.length - 1];
+}
 
-//const getLastName= ([name1,name2,name3]);
-//const names = [
-  //  name[0]= name1;
-  //  name[1]= name2;
-   // name[2]= name3]
-
-//for (let index= 0; index <3; index ++) {
- //   console.log(index[2])
-//}
+console.log(getLastName(["Charlie", "Rob", "Andy"]));
+console.log(getLastName(["Ash", "Stu",]));
 
 
-//getLastname(["Charlie", "Rob", "Andy"]);
-//getLastname(["Ash", "Stu",]);
+// or ...
+
+const getLastNameRecursively = (nameArr) => {
+
+    let nameToReturn = "";
+    for (let index = 0; index < nameArr.length; index++) {
+        nameToReturn = nameArr[index];
+    }
+    return nameToReturn;
+}
+
+console.log(getLastNameRecursively(["Charlie", "Rob", "Andy"]));
+console.log(getLastNameRecursively(["Ash", "Stu",]));
+
+
 
 // 5. function which takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are 
 // one or more negative numbers in the array. 
+
+const allNUmbersPositive = (numberArr) => {
+    for( let index = 0; index < numberArr.length; index++) {
+        if (numberArr[index] < 0) {
+            return false; 
+        }
+    }
+    return true;
+}
+
+console.log(allNUmbersPositive([2,4,5]))
+console.log(allNUmbersPositive([-5,4,6]))
